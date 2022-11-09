f96e113f98fa7d67a3438b77ef756768ef7fed10
LauraCordoba1 <laurabelen.cordoba@hotmail.com>
2022-11-09 20:28:31 -0300

    nuevo repo

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+
+let port = process.env.PORT || 3000;
+
+app.use(express.static("public"))
+
+let PORT = 3000
+app.listen(PORT, console.log("listen on port" + PORT))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve("./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve("./views/login.html"));
+})
